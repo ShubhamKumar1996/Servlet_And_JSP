4344242abefc5b7e03feac59a9744afa9150819b
ShubhamKumar1996 <Nimcetshubhamkumar@gmail.com>
2021-11-18 20:08:42 +0530

    Creating first servlet by implementing Servlet Interface

---
+++ src/main/java/com/example/servlets/FirstServlet.java
@@ -0,0 +1,40 @@
+package com.example.servlets;
+
+import java.io.*;
+
+import jakarta.servlet.*;
+
+
+
+public class FirstServlet implements Servlet {
+	
+	// Lifecycle Method
+	ServletConfig conf;
+	
+	public void init(ServletConfig conf) {
+		System.out.println("Creating...................");
+		this.conf = conf;
+	}
+	
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		res.setContentType("text/html");
+		PrintWriter printWriter = res.getWriter();
+		printWriter.println("Hello World");
+		printWriter.println("<h1>Hello World!!!</h1>");
+		System.out.println("Servicing.....................");
+	}
+	
+	public void destroy() {
+		System.out.println("Destroying.....................");
+	}
+	
+	// Non-Lifecycle Method
+	
+	public ServletConfig getServletConfig() {
+		return this.conf;
+	}
+	
+	public String getServletInfo() {
+		return "This is a servlet";
+	}
+}
